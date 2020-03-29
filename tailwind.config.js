f48b4a9460edabbc3298b1b9f1fa6a41f7cb04bc
Fa-BRAIK <faroukbraik17@gmail.com>
2020-03-29 17:42:12 -0400

    adding tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+//  this is the file where we can change global variables and styles
+
+module.exports = {
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
